420202c34b5f782cb140d94b31e9c4f2c5dab25d
David My <ducmy@MacBook-Air-cua-David.local>
2022-03-15 00:26:57 +0700

    lesson 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="icon" href="https://eupgroup.net/favicon_eup.svg" type="image/ico">
+    <title>Khóa học Laravel cơ bản đến nâng cáo 2022</title>
 </head>
+<style>
+    .laravel_class {
+        text-align: center;
+        color: red;
+    }
+</style>
 
 <body>
     <div class="">
-        <h1 class="">Khóa học Laravel One Own One</h1>
+        <h1 class="laravel_class">Khóa học Laravel 2022</h1>
+        <p style="text-align:center">
+            Owner: Ng Duc My<br> Supporter: Minh Quang <br> <br> Stutent: Loi
+        </p>
     </div>
 </body>
 
